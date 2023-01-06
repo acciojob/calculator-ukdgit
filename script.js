f7fc728b835ea35bceb1d6d463576427b518f9d6
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-06 18:51:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//your code here
+let num = '';
+  let answer=''
+  // $('button').click(()=>{
+  //  console.log($('button').attr('value'))
+  // })
+  function add(value) {
+  console.log(value)
+  num = num+value;
+  document.getElementById('input').value = num;
+  }
+  function ans() {
+  answer = eval(num);
+  document.getElementById('input').value = answer;
+  answer=''
+  num=''
+  }
+  function clr(){
+  num='';
+  document.getElementById('input').value = "";
+  }
